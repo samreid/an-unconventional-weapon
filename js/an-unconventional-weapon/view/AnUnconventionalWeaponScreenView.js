91da169223a0c81cc0e6c64cbc2083ffcffa5c67
Sam Reid <reids@colorado.edu>
2015-04-18 14:57:53 -0600

    reordered levels

--- js/an-unconventional-weapon/view/AnUnconventionalWeaponScreenView.js
+++ js/an-unconventional-weapon/view/AnUnconventionalWeaponScreenView.js
@@ -14,7 +14,7 @@ define( function( require ) {
   var LevelGun = require( 'AN_UNCONVENTIONAL_WEAPON/an-unconventional-weapon/view/LevelGun' );
   var ScreenView = require( 'JOIST/ScreenView' );
 
-  var levels = [ LevelGun, LevelSword ];
+  var levels = [ LevelSword, LevelGun ];
   var levelIndex = 0;
 
   /**
